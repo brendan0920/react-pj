321ce8b7d853b0144bc9f11b8a5cc339179a0aa2
brendan0920 <brdk1220@gmail.com>
2024-11-19 17:38:12 -0500

    init

---
+++ rts/src/index.tsx
@@ -0,0 +1,32 @@
+// import ReactDOM from "react-dom";
+
+// const App = () => {
+//   return (
+//     <div>
+//       <h1>Hi there!</h1>
+//     </div>
+//   );
+// };
+
+// ReactDOM.render(<App />, document.querySelector("#root"));
+
+// 1) Import ReactDOM library
+import ReactDOM from "react-dom/client";
+
+// 2) Get a reference to the div with ID root
+const el = document.getElementById("root");
+
+// 3) Tell React to take control of that element
+const root = ReactDOM.createRoot(el!);
+
+// 4) Create a component
+const App = () => {
+  return (
+    <div>
+      <h1>Hi there!</h1>
+    </div>
+  );
+};
+
+// 5) Show the component on the screen
+root.render(<App />);
