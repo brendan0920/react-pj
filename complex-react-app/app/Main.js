dfb52ea9039bf7c1efaa8f838054a1904b1e0d1a
brendan0920 <brdk1220@gmail.com>
2024-11-19 17:19:31 -0500

    init

---
+++ complex-react-app/app/Main.js
@@ -0,0 +1,88 @@
+import React, { useState, useEffect, useReducer } from "react";
+import ReactDOM from "react-dom/client";
+import { useImmerReducer } from "use-immer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Axios from "axios";
+Axios.defaults.baseURL = "http://localhost:8080";
+
+import StateContext from "./StateContext";
+import DispatchContext from "./DispatchContext";
+
+import Header from "./components/Header";
+import HomeGuest from "./components/HomeGuest";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import Terms from "./components/Terms";
+import CreatePost from "./components/CreatePost";
+import ViewSinglePost from "./components/ViewSinglePost";
+import FlashMessages from "./components/FlashMessages";
+import Profile from "./components/Profile";
+
+function Main() {
+  const initialState = {
+    loggedin: Boolean(localStorage.getItem("complexappToken")),
+    flashMessages: [],
+    user: {
+      token: localStorage.getItem("complexappToken"),
+      username: localStorage.getItem("complexappUsername"),
+      avatar: localStorage.getItem("complexappAvatar")
+    }
+  };
+
+  function ourReducer(draft, action) {
+    switch (action.type) {
+      case "login":
+        draft.loggedIn = true;
+        draft.user = action.data;
+        break;
+      case "logout":
+        draft.loggedIn = false;
+        break;
+      case "flashMessage":
+        draft.flashMessages.push(action.value);
+        break;
+    }
+  }
+
+  const [state, dispatch] = useImmerReducer(ourReducer, initialState);
+
+  useEffect(() => {
+    if (state.loggedIn) {
+      localStorage.setItem("complexappToken", state.user.token);
+      localStorage.setItem("complexappUsername", state.user.username);
+      localStorage.setItem("complexappAvatar", state.user.avatar);
+    } else {
+      localStorage.removeItem("complexappToken");
+      localStorage.removeItem("complexappUsername");
+      localStorage.removeItem("complexappAvatar");
+    }
+  }, [state.loggedin]);
+
+  return (
+    <StateContext.Provider value={state}>
+      <DispatchContext.Provider value={dispatch}>
+        <BrowserRouter>
+          <FlashMessages messages={state.flashMessages} />
+          <Header />
+          <Routes>
+            <Route path="/profile/:username/*" element={<Profile />} />
+            <Route path="/" element={state.loggedIn ? <Home /> : <HomeGuest />} />
+            <Route path="/post/:id" element={<ViewSinglePost />} />
+            <Route path="/create-post" element={<CreatePost />} />
+            <Route path="/about-us" element={<About />} />
+            <Route path="/terms" element={<Terms />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </DispatchContext.Provider>
+    </StateContext.Provider>
+  );
+}
+
+const root = ReactDOM.createRoot(document.querySelector("#app"));
+root.render(<Main />);
+
+if (module.hot) {
+  module.hot.accept();
+}
---
+++ complex-react-app/app/components/s.js
@@ -0,0 +1,1 @@
+// mongodb+srv://brdk1220:Bl02122015092088.@cluster0.ccou4.mongodb.net/ReactCourse?retryWrites=true&w=majority&appName=Cluster0
