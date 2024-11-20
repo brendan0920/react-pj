eef29dbaae9dfd383086be6bf852d06acaf06399
brendan0920 <brdk1220@gmail.com>
2024-11-20 18:32:24 -0500

    git init

---
+++ globomantics/pages/_document.js
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+        <link rel="stylesheet" href="css/globals.css" />
+      </Head>
+      <body>
+        <div className="container">
+          <Main />
+        </div>
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
